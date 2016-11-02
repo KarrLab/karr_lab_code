1b4b4ae7e4f5c2610395cd555a862f7e1d569cd5
Jonathan Karr <jonrkarr@gmail.com>
2016-11-02 09:34:59 -0700

    debugging for chron job

--- save_github_traffic_info.py
+++ save_github_traffic_info.py
@@ -8,18 +8,20 @@ import os
 
 #settings
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
+TOKENS_DIR = os.path.join(os.path.dirname(__file__), 'tokens')
+REPOS_DIR = os.path.join(os.path.dirname(__file__), 'repo')
 
-with open('tokens/GITHUB_USERNAME', 'r') as file:
+with open(os.path.join(TOKENS_DIR, 'GITHUB_USERNAME'), 'r') as file:
     GITHUB_USERNAME = file.read().rstrip()
 
-with open('tokens/GITHUB_PASSWORD', 'r') as file:
+with open(os.path.join(TOKENS_DIR, 'GITHUB_PASSWORD'), 'r') as file:
     GITHUB_PASSWORD = file.read().rstrip()
 
-with open('tokens/GITHUB_ACCESS_TOKEN', 'r') as file:
+with open(os.path.join(TOKENS_DIR, 'GITHUB_ACCESS_TOKEN'), 'r') as file:
     GITHUB_ACCESS_TOKEN = file.read().rstrip()
 
 #download view and clone counts
-for repo_config_filename in glob('repo/*.json'):
+for repo_config_filename in glob(os.path.join(REPOS_DIR, '*.json')):
     with open(repo_config_filename, 'r') as file:
         config = json.load(file)
 
@@ -71,7 +73,7 @@ for repo_config_filename in glob('repo/*.json'):
         unique_clones = 0
 
     #log results
-    filename = os.path.join('repo', '{}.stats.tsv'.format(repo))
+    filename = os.path.join(REPOS_DIR, '{}.stats.tsv'.format(repo))
 
     if not os.path.isfile(filename):
         with open(filename, 'a') as file:
